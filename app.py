5c3e942131d3798159674564d9acc76cbf9daa4f
vguillotduenas-dotcom <vguillotduenas@gmail.com>
2026-03-14 14:44:36 +0100

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, current_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'netflix-clone-super-secret'
+app.config['SECRET_KEY'] = 'ma-cle-ultra-secrete'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-# --- MODÈLES ---
+# --- MODELES ---
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nom = db.Column(db.String(100))
@@ -26,7 +26,7 @@ class Video(db.Model):
     img = db.Column(db.String(500))
     lien = db.Column(db.String(500))
     type = db.Column(db.String(20))
-    genre = db.Column(db.String(50), default='Autre')
+    genre = db.Column(db.String(50))
     episodes = db.relationship('Episode', backref='video', cascade="all, delete-orphan")
 
 class Episode(db.Model):
@@ -51,19 +51,17 @@ with app.app_context():
 @login_required
 def index():
     q = request.args.get('q')
-    g = request.args.get('genre')
+    genre_f = request.args.get('genre')
     query = Video.query
-    if q:
-        query = query.filter(Video.nom.contains(q))
-    if g:
-        query = query.filter(Video.genre == g)
+    if q: query = query.filter(Video.nom.contains(q))
+    if genre_f: query = query.filter(Video.genre == genre_f)
     return render_template('index.html', films=query.all())
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        code_saisi = request.form.get('code').strip()
-        user = User.query.filter_by(code=code_saisi).first()
+        code_s = request.form.get('code').strip()
+        user = User.query.filter_by(code=code_s).first()
         if user:
             if user.role == 'user' and not user.nom:
                 user.nom, user.prenom = request.form.get('nom'), request.form.get('prenom')
@@ -72,28 +70,25 @@ def login():
             return redirect(url_for('index'))
     return render_template('login.html')
 
-@app.route('/serie/<int:id>')
-@login_required
-def serie_details(id):
-    serie = Video.query.get_or_404(id)
-    return render_template('serie.html', serie=serie)
-
 @app.route('/admin', methods=['GET', 'POST'])
 @login_required
 def admin():
     if current_user.role != 'admin': return redirect(url_for('index'))
     if request.method == 'POST':
         if 'add_video' in request.form:
-            v = Video(nom=request.form['nom'], img=request.form['img'], lien=request.form['lien'], type=request.form['type'], genre=request.form['genre'])
-            db.session.add(v)
+            db.session.add(Video(nom=request.form['nom'], img=request.form['img'], lien=request.form['lien'], type=request.form['type'], genre=request.form['genre']))
         elif 'add_episode' in request.form:
-            ep = Episode(video_id=request.form['video_id'], saison=request.form['saison'], titre_ep=request.form['titre'], lien_ep=request.form['lien_ep'])
-            db.session.add(ep)
+            db.session.add(Episode(video_id=request.form['video_id'], saison=request.form['saison'], titre_ep=request.form['titre'], lien_ep=request.form['lien_ep']))
         elif 'gen_code' in request.form:
             db.session.add(User(code=str(uuid.uuid4())[:8].upper()))
         db.session.commit()
         return redirect(url_for('admin'))
     return render_template('admin.html', films=Video.query.all(), users=User.query.filter_by(role='user').all())
+
+@app.route('/serie/<int:id>')
+@login_required
+def serie_details(id):
+    return render_template('serie.html', serie=Video.query.get_or_404(id))
 
 @app.route('/del_v/<int:id>')
 @login_required
@@ -110,5 +105,4 @@ def del_u(id):
     return redirect(url_for('admin'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
