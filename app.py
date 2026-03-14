90dff4572181ab3fde9daff03bfa7e606aa2fe31
vguillotduenas-dotcom <vguillotduenas@gmail.com>
2026-03-14 10:57:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -95,5 +95,6 @@ if __name__ == '__main__':
         if not User.query.filter_by(is_admin=True).first():
             db.session.add(User(password='ADMIN123', is_admin=True))
             db.session.commit()
+    # Configuration vitale pour Render
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
