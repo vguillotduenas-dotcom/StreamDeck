bea8cd52a4813cde0f3ba1b89c635af180fc2922
vguillotduenas-dotcom <vguillotduenas@gmail.com>
2026-03-15 14:28:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -110,4 +110,5 @@ def del_u(id):
     return redirect(url_for('admin'))
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
